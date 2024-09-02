ee97bbe0cffd3a369308e138a235251ad2ae8c6b
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-09-02 17:36:40 +0100

    Add #correct_notation? method in notation module that checks whether a given notation is correct or not

--- lib/notation.rb
+++ lib/notation.rb
@@ -1,2 +1,18 @@
 module Notation
+  NOTATION = /^(?<piece>(?<_>[KBNQR][a-h]?[1-8]?)|[a-h])?(?:(?<=[a-h])x|x?)(?<final_position>[a-h][1-8])(?<promotion>=?[KBNQR])?$/.freeze
+  CASTLE_NOTATION = /^O-O(?:-O)?$/.freeze
+  def notation_to_index(algebraic)
+  end
+
+  def correct_notation?(algebraic)
+    return false unless algebraic.is_a? String
+
+    algebraic.match?(NOTATION) || algebraic.match?(CASTLE_NOTATION)
+  end
+
+  def file_to_index
+  end
+
+  def rank_to_index
+  end
 end
